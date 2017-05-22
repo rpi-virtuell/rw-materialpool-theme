d5725ff017870d12d9e97268461a3d956993d757
Joachim Happel <joachim.happel@gmail.com>
2017-05-22 15:01:41 +0200

    add footer section

--- footer.php
+++ footer.php
@@ -18,10 +18,23 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="wrap">
-                Footer
-				<?php
-				//get_template_part( 'template-parts/footer/site', 'info' );
-				?>
+                <?php
+                get_template_part( 'template-parts/footer/footer', 'widgets' );
+
+                if ( has_nav_menu( 'social' ) ) : ?>
+                <nav class="social-navigation" role="navigation" aria-label="<?php _e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
+                    <?php
+                    wp_nav_menu( array(
+                        'theme_location' => 'social',
+                        'menu_class'     => 'social-links-menu',
+                        'depth'          => 1,
+                        'link_before'    => '<span class="screen-reader-text">',
+                        'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
+                    ) );
+                    ?>
+                </nav><!-- .social-navigation -->
+                <?php endif; ?>
+
 			</div><!-- .wrap -->
 		</footer><!-- #colophon -->
 	</div><!-- .site-content-contain -->
