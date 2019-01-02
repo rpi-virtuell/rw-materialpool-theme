1599541efd949d7e287eaecd0f77ec7c599fc85f
Frank Staude <frank@staude.net>
2019-01-02 21:45:04 +0100

    Materialdetail - weitere Materialien und verweise
    Materialdetail - weitere Materialien und verweise

--- template-parts/material/content-part-links.php
+++ template-parts/material/content-part-links.php
@@ -7,9 +7,53 @@
  */
 ?>
 <?php if ( Materialpool_Material::has_verweise() ) : ?>
-    <div class="material-detail-verweise material-links">
+    <div class="material-detail-verweise material-links ">
         <h4>Siehe dazu auch folgendes Material:</h4>
-        <?php echo facetwp_display( 'template', 'material_verweise' ); ?>
+
+        <div class="startseite-block-content material-results">
+            <div class="facetwp-template">
+			    <?php
+			    $ar = Materialpool_Material::get_verweise_ids();
+			    if ( $ar === false ) return;
+			    $args = array(
+				    'post__in'                      => $ar,
+				    'post_type'              => array( 'material' ),
+			    );
+			    $my_query = new WP_Query( $args );
+			    while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+                    <div class="facet-treffer<?php echo (Materialpool_Material::is_alpika())?' alpika':'';?><?php echo (Materialpool_Material::is_special())?' special':'';?>">
+                        <div class="facet-treffer-content">
+						    <?php if (Materialpool_Material::cover_facet_html()  && !in_array(strrchr(Materialpool_Material::get_url(),'.'),array('.docx','.doc','.odt')) ):?>
+                                <div class="material-cover">
+								    <?php echo  Materialpool_Material::cover_facet_html(); ?>
+                                </div>
+						    <?php endif; ?>
+                            <p class="material-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+                            <p class="search-description">
+							    <?php Materialpool_Material::shortdescription(); ?><br>
+							    <?php //echo wp_trim_words(  wp_strip_all_tags ( Materialpool_Material::get_description() )) ; ?>
+                            </p>
+                            <p class="search-head">
+							    <?php if(Materialpool_Material::get_organisation()[0]){
+								    echo Materialpool_Material::organisation_facet_html().'<br>';
+							    }
+							    if(Materialpool_Material::get_autor()) {
+								    echo Materialpool_Material::autor_facet_html();
+							    }
+							    ?>
+                            </p>
+                        </div><div class="clear"></div>
+
+                    </div>
+			    <?php endwhile;
+			    wp_reset_postdata();
+			    unset( $my_query);
+
+			    ?>
+
+
+            </div>
+        </div>
     </div>
 <?php endif; ?>
 
@@ -22,9 +66,53 @@
 <?php endif; ?>
 
 <?php if ( Materialpool_Material::is_werk() ) : ?>
-    <div class="material-detail-children material-links">
+    <div class="material-detail-children material-links home">
         <h4>Zu "<?php the_title( '<b>', '</b>' );?>" gehören weitere Materialien:</h4>
-        <?php Materialpool_Material::volumes_html( true ); ?>
+
+        <div class="startseite-block-content material-results">
+            <div class="facetwp-template">
+			    <?php
+			    $ar = Materialpool_Material::volumes_ids();
+			    if ( $ar === false ) return;
+			    $args = array(
+				    'post__in'                      => $ar,
+				    'post_type'              => array( 'material' ),
+			    );
+			    $my_query = new WP_Query( $args );
+			    while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+                <div class="facet-treffer<?php echo (Materialpool_Material::is_alpika())?' alpika':'';?><?php echo (Materialpool_Material::is_special())?' special':'';?>">
+                    <div class="facet-treffer-content">
+					    <?php if (Materialpool_Material::cover_facet_html()  && !in_array(strrchr(Materialpool_Material::get_url(),'.'),array('.docx','.doc','.odt')) ):?>
+                            <div class="material-cover">
+							    <?php echo  Materialpool_Material::cover_facet_html(); ?>
+                            </div>
+					    <?php endif; ?>
+                        <p class="material-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+                        <p class="search-description">
+						    <?php Materialpool_Material::shortdescription(); ?><br>
+						    <?php //echo wp_trim_words(  wp_strip_all_tags ( Materialpool_Material::get_description() )) ; ?>
+                        </p>
+                        <p class="search-head">
+						    <?php if(Materialpool_Material::get_organisation()[0]){
+							    echo Materialpool_Material::organisation_facet_html().'<br>';
+						    }
+						    if(Materialpool_Material::get_autor()) {
+							    echo Materialpool_Material::autor_facet_html();
+						    }
+						    ?>
+                        </p>
+                    </div><div class="clear"></div>
+
+                </div>
+                <?php endwhile;
+	                    wp_reset_postdata();
+	                    unset( $my_query);
+
+	                    ?>
+
+
+            </div>
+        </div>
     </div>
 
 <?php endif; ?>
