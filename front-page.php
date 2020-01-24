78481f287723e426b40c4b09821c449735be3482
Frank Staude <frank@staude.net>
2020-01-24 12:25:15 +0100

    acf migration

--- front-page.php
+++ front-page.php
@@ -15,17 +15,17 @@ global $post;
         <div id="primary" class="content-area">
             <main id="main" class="site-main" role="main">
                 <?php
-                $show_title = get_metadata( 'post', $post->ID, 'show_title', true );
+                $show_title = get_field( 'show_title');
                 if ( $show_title == 1 ) {
                 ?>
                 <div class="startseite-block-header">
                     <p><?php
-	                    $startseite_title = get_metadata( 'post', $post->ID, 'startseite_title', true );
+	                    $startseite_title = get_field( 'startseite_title' );
 	                    echo do_shortcode( $startseite_title );
 	                    ?></p>
                     <div class="startseite-block-content">
                         <?php
-                        $freitext = get_metadata( 'post', $post->ID, 'startseite_freitext', true );
+                        $freitext = get_field( 'startseite_freitext' );
                         echo do_shortcode( $freitext );
                         ?>
                     </div>
@@ -36,79 +36,90 @@ global $post;
                 <div class="clear"></div>
 
 	            <?php
-	            $show_aktuell = get_metadata( 'post', $post->ID, 'show_aktuell', true );
+	            $show_aktuell = get_field( 'show_aktuell' );
 	            if ( $show_aktuell == 1 ) {
 		            ?>
                 <div class="startseite-block-header">
                     <p><?php
-	                    $startseite_aktuell_titel = get_metadata( 'post', $post->ID, 'startseite_aktuell_titel', true );
+	                    $startseite_aktuell_titel = get_field( 'startseite_aktuell_titel' );
 	                    echo do_shortcode( $startseite_aktuell_titel );
 	                    ?></p>
                     <div class="startseite-block-content material-results">
                         <div class="facetwp-template" data-name="startseite_aktuell">
                         <?php
-                        $startseite_aktuell = get_metadata( 'post', $post->ID, 'startseite_aktuell', false );
-                        $IDlistArr = array();
-                        foreach ( $startseite_aktuell as $entry ) {
-                            $IDlistArr[] = $entry['ID'];
-                        }
-	                    $args = array(
-		                    'post__in'                      => $IDlistArr ,
-		                    'post_type'              => array( 'material' ),
-	                    );
-	                    $my_query = new WP_Query( $args );
-                        while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
-                            <div class="facet-treffer<?php echo (Materialpool_Material::is_alpika())?' alpika':'';?><?php echo (Materialpool_Material::is_special())?' special':'';?>">
-                                <div class="facet-treffer-content">
-				                    <?php if (Materialpool_Material::cover_facet_html()  && !in_array(strrchr(Materialpool_Material::get_url(),'.'),array('.docx','.doc','.odt')) ):?>
-                                        <div class="material-cover">
-						                    <?php echo  Materialpool_Material::cover_facet_html(); ?>
-                                        </div>
-				                    <?php endif; ?>
-                                    <p class="material-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-                                    <p class="search-description">
-					                    <?php Materialpool_Material::shortdescription(); ?><br>
-					                    <?php //echo wp_trim_words(  wp_strip_all_tags ( Materialpool_Material::get_description() )) ; ?>
-                                    </p>
-                                    <p class="search-head">
-					                    <?php if(Materialpool_Material::get_organisation()[0]){
-						                    echo Materialpool_Material::organisation_facet_html().'<br>';
-					                    }
-					                    if(Materialpool_Material::get_autor()) {
-						                    echo Materialpool_Material::autor_facet_html();
-					                    }
-					                    ?>
-                                    </p>
-                                </div><div class="clear"></div>
+                        $startseite_aktuell = get_field( 'startseite_aktuell' );
+		                if ( $startseite_aktuell !== null && $startseite_aktuell != '' ) {
+			                $IDlistArr = array();
+			                foreach ( $startseite_aktuell as $entry ) {
+				                $IDlistArr[] = $entry->ID;
+			                }
 
-                            </div>
-	                    <?php endwhile;
-	                    wp_reset_postdata();
-	                    unset( $my_query);
-                        $startseite_aktuell = get_metadata( 'post', $post->ID, 'startseite_themen', false );
-                        $IDlistArr = array();
-                        foreach ( $startseite_aktuell as $entry ) {
-	                        $IDlistArr[] = $entry['ID'];
+			                $args     = array(
+				                'post__in' => $IDlistArr,
+				                'post_type' => array( 'material' ),
+			                );
+			                $my_query = new WP_Query( $args );
+			                while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+                                <div class="facet-treffer<?php echo ( Materialpool_Material::is_alpika() ) ? ' alpika' : ''; ?><?php echo ( Materialpool_Material::is_special() ) ? ' special' : ''; ?>">
+                                    <div class="facet-treffer-content">
+						                <?php if ( Materialpool_Material::cover_facet_html() && ! in_array( strrchr( Materialpool_Material::get_url(), '.' ), array(
+								                '.docx',
+								                '.doc',
+								                '.odt'
+							                ) ) ): ?>
+                                            <div class="material-cover">
+								                <?php echo Materialpool_Material::cover_facet_html(); ?>
+                                            </div>
+						                <?php endif; ?>
+                                        <p class="material-title"><a
+                                                    href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+                                        <p class="search-description">
+							                <?php Materialpool_Material::shortdescription(); ?><br>
+							                <?php //echo wp_trim_words(  wp_strip_all_tags ( Materialpool_Material::get_description() )) ; ?>
+                                        </p>
+                                        <p class="search-head">
+							                <?php if ( Materialpool_Material::get_organisation()[0] ) {
+								                echo Materialpool_Material::organisation_facet_html() . '<br>';
+							                }
+							                if ( Materialpool_Material::get_autor() ) {
+								                echo Materialpool_Material::autor_facet_html();
+							                }
+							                ?>
+                                        </p>
+                                    </div>
+                                    <div class="clear"></div>
+
+                                </div>
+			                <?php endwhile;
+			                wp_reset_postdata();
+			                unset( $my_query );
+		                }
+                        $startseite_aktuell = get_field( 'startseite_themen');
+                        if ( $startseite_aktuell !== null && $startseite_aktuell != '' ) {
+                            $IDlistArr = array();
+                            foreach ( $startseite_aktuell as $entry ) {
+                                $IDlistArr[] = $entry->ID;
+                            }
+                            $args = array(
+                                'post__in'                      => $IDlistArr ,
+                                'post_type'              => array( 'themenseite' ),
+                            );
+                            $my_query = new WP_Query( $args );
+                            while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+                                <div class="facet-treffer>">
+                                    <div class="facet-treffer-content">
+                                            <div class="material-cover">
+                                                <img src="<?php echo catch_thema_image() ?>">
+                                            </div>
+                                        <p class="material-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+                                        <p class="search-description">
+                                            <?php the_excerpt(); ?>
+                                        </p>
+                                    </div><div class="clear"></div>
+                                </div>
+                            <?php endwhile;
+                            wp_reset_postdata();
                         }
-                        $args = array(
-	                        'post__in'                      => $IDlistArr ,
-	                        'post_type'              => array( 'themenseite' ),
-                        );
-                        $my_query = new WP_Query( $args );
-                        while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
-                            <div class="facet-treffer>">
-                                <div class="facet-treffer-content">
-                                        <div class="material-cover">
-                                            <img src="<?php echo catch_thema_image() ?>">
-                                        </div>
-                                    <p class="material-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-                                    <p class="search-description">
-	                                    <?php the_excerpt(); ?>
-                                    </p>
-                                </div><div class="clear"></div>
-                            </div>
-                        <?php endwhile;
-                        wp_reset_postdata();
 	                    ?>
                         </div>
                     </div>
@@ -118,12 +129,12 @@ global $post;
 	            }
 	            ?>
 	            <?php
-	            $show_neu = get_metadata( 'post', $post->ID, 'show_neu', true );
+	            $show_neu = get_field( 'show_neu' );
 	            if ( $show_neu == 1 ) {
 		            ?>
                 <div class="startseite-block-header">
                     <P><?php
-	                    $startseite_neu_titel = get_metadata( 'post', $post->ID, 'startseite_neu_titel', true );
+	                    $startseite_neu_titel = get_field( 'startseite_neu_titel' );
 	                    echo do_shortcode( $startseite_neu_titel );
 	                    ?></P>
                     <div class="startseite-block-content  material-results">
@@ -135,12 +146,12 @@ global $post;
 	            }
 	            ?>
 	            <?php
-	            $show_themenseiten = get_metadata( 'post', $post->ID, 'show_themenseiten', true );
+	            $show_themenseiten = get_field( 'show_themenseiten' );
 	            if ( $show_themenseiten == 1 ) {
 		            ?>
                     <div class="startseite-block-header">
                         <P><?php
-				            $show_themenseiten_titel = get_metadata( 'post', $post->ID, 'startseite_themenseiten_titel', true );
+	                        $show_themenseiten_titel = get_field( 'startseite_themenseiten_titel' );
 				            echo do_shortcode( $show_themenseiten_titel );
 				            ?></P>
                         <div class="startseite-block-content  material-results">
@@ -178,12 +189,12 @@ global $post;
 
 
 	            <?php
-	            $show_oer = get_metadata( 'post', $post->ID, 'show_oer', true );
+	            $show_oer = get_field( 'show_oer');
 	            if ( $show_oer == 1 ) {
 		            ?>
                 <div class="startseite-block-header">
                     <P><?php
-	                    $startseite_oer_titel = get_metadata( 'post', $post->ID, 'startseite_oer_titel', true );
+                        $startseite_oer_titel = get_field( 'startseite_oer_titel' );
 	                    echo do_shortcode( $startseite_oer_titel );
 	                    ?></P>
                     <div class="startseite-block-content  material-results">
@@ -195,12 +206,12 @@ global $post;
 	            }
 	            ?>
 	            <?php
-	            $show_special = get_metadata( 'post', $post->ID, 'show_special', true );
+	            $show_special = get_field( 'show_special' );
 	            if ( $show_special == 1 ) {
 		            ?>
                 <div class="startseite-block-header">
                     <p><?php
-	                    $startseite_special_titel = get_metadata( 'post', $post->ID, 'startseite_special_titel', true );
+	                    $startseite_special_titel = get_field( 'startseite_special_titel' );
 	                    echo do_shortcode( $startseite_special_titel );
 	                    ?></p>
                     <div class="startseite-block-content material-results">
@@ -212,17 +223,17 @@ global $post;
 	            }
 	            ?>
 	            <?php
-	            $show_about = get_metadata( 'post', $post->ID, 'show_about', true );
+	            $show_about = get_field( 'show_about' );
 	            if ( $show_about == 1 ) {
 		            ?>
                 <div class="startseite-block-header">
                     <p><?php
-	                    $startseite_about_titel = get_metadata( 'post', $post->ID, 'startseite_about_titel', true );
+	                    $startseite_about_titel = get_field( 'startseite_about_titel' );
 	                    echo do_shortcode( $startseite_about_titel );
 	                    ?></p>
                     <div class="startseite-block-content">
                         <?php
-                        $about = get_metadata( 'post', $post->ID, 'startseite_about', true );
+                        $about = get_field( 'startseite_about' );
                         echo do_shortcode( $about );
                         ?>
                     </div>
